1928b3235cb7a209d77d929f8eea9b9d6f99517e
magskwa <magali.egger@heig-vd.ch>
2022-10-07 19:22:20 +0200

    add some features

--- main/client/client.go
+++ main/client/client.go
@@ -8,18 +8,24 @@ import (
 	"os"
 )
 
-var servAddr = "127.0.0.1:8080"
+// counters
+var eventCounter int = 0
+var postCounter int = 0
+var userCounter int = 0
 
-func RunClient(id string) {
-	conn, err := net.Dial("tcp", servAddr)
+func main() {
+	helpMenu()
+
+	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-	done := make(chan string)
+
+	done := make(chan struct{})
 	go func() {
 		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
 		log.Println("done")
-		done <- "bsus" // signal the main goroutine
+		done <- struct{}{} // signal the main goroutine
 	}()
 	if _, err := io.Copy(conn, os.Stdin); err != nil {
 		log.Fatal(err)
@@ -28,6 +34,13 @@ func RunClient(id string) {
 	<-done // wait for background goroutine to finish
 }
 
-func main() {
-	fmt.Println("Coucou")
+func helpMenu() {
+	fmt.Println("Hello and Welcome to the Event Manager")
+	fmt.Println("Here is a list of possible commands:")
+	fmt.Println("1. CREATE eventName postName1 capacity1 postName2 capacity2 ... ")
+	fmt.Println("2. CLOSE eventName")
+	fmt.Println("3. Add a benevole to a post")
+	fmt.Println("4. List all the manifestations")
+	fmt.Println("5. List all the posts of a manifestation")
+	fmt.Println("6. List all the benevoles of a manifestation")
 }
--- main/server/server.go
+++ main/server/server.go
@@ -51,6 +51,8 @@ var posts []Post
 // array of users
 var users []User
 
+var isAuthentified = false
+
 func main() {
 
 	// Creating a simple TCP server
@@ -60,13 +62,13 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// manage concurrency
-	//done := make(chan struct{})
 
 	fmt.Println("Server is listening on port 8080")
 
 	// close the listener when the application closes
 	defer listen.Close()
+
+	createUsers()
 
 	// accept connection on port
 	for {
@@ -77,13 +79,17 @@ func main() {
 
 		// call the handleRequest function
 		go handleRequest(conn)
-		// todo done ?
 	}
-
-	//<-done
 }
 
 func handleRequest(conn net.Conn) {
+	// ask for the username and password
+	for !isAuthentified {
+		authentification()
+	}
+
+	// reinitialize the authentification
+	isAuthentified = false
 
 	// make a buffer to hold incoming data
 	buf := make([]byte, 1024)
@@ -119,7 +125,11 @@ func parseBuffer(buf []byte) {
 	// do a switch on the first element of slice
 	switch slice[0] {
 	case "CREATE":
-		fmt.Println("START")
+		fmt.Println("Starting an event")
+		for !isAuthentified {
+			authentification()
+		}
+		isAuthentified = false
 	case "CLOSE":
 		fmt.Println("CLOSE")
 	case "ADD":
@@ -137,8 +147,40 @@ func parseBuffer(buf []byte) {
 
 // Create a few users
 func createUsers() {
-	users = append(users, User{"Michelle", 1234, Post{}})
+	events = append(events, Event{1, "Festival de la bière", 1})
+	posts = append(posts, Post{1, "Post1", 3, events[0]})
+	posts = append(posts, Post{2, "Post2", 2, events[0]})
+	users = append(users, User{"Ficelle", 1234, Post{}})
 	users = append(users, User{"Taro", 1234, Post{}})
-	users = append(users, User{"Catanne", 1234, Post{}})
-	users = append(users, User{"Willi", 1234, Post{}})
+	users = append(users, User{"Catanne", 1234, posts[0]})
+	users = append(users, User{"Willi", 1234, posts[1]})
+}
+
+func authentification() {
+	// ask for the username
+	fmt.Println("Please enter your username : ")
+	var username string
+	fmt.Scanln(&username)
+
+	// check if the username exists
+	for _, user := range users {
+		if user.name == username {
+
+			// ask for the password
+			fmt.Println("Please enter your password : ")
+			var password int
+			fmt.Scanln(&password)
+
+			// check if the password is correct
+			if user.password == password {
+				isAuthentified = true
+				fmt.Println("Authentification successful")
+				return
+			} else {
+				fmt.Println("Wrong password")
+			}
+		}
+	}
+	fmt.Println("Wrong username")
+	authentification()
 }
