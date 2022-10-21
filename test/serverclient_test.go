7881b1f6712f7eeaa60e73f95863fcf874f47ac6
NunezTania <tnunez@bluewin.ch>
2022-10-21 11:16:31 +0200

    Début des tests

--- test/serverclient_test.go
+++ test/serverclient_test.go
@@ -1,38 +1,66 @@
-// The test package contains the tests of the program.
-// It acts as a client and sends commands to the server.
-// The server will then process the commands and send back the result.
 package test
 
 import (
+	"SDR-Laboratoire1/main/server"
 	"bytes"
 	"fmt"
-	"os"
-	"os/exec"
+	"log"
+	"net"
 	"testing"
 )
 
+const (
+	HOST = "localhost"
+	PORT = "5555"
+	TYPE = "tcp"
+)
+
 func Test(t *testing.T) {
-	fmt.Println("Test")
-
-	go func() {
-		p := exec.Command("go", "run", "../main/server/server.go")
-		p.Stdout = os.Stdout
-		p.Stderr = os.Stderr
-		err := p.Run()
-		if err != nil {
-			fmt.Println(err)
-		}
-	}()
-
-	p := exec.Command("go", "run", "../main/client/client.go")
-	buffer := bytes.Buffer{}
-	buffer.Write([]byte("LISTM\n"))
-	buffer.Write([]byte("QUIT\n"))
-	p.Stdin = &buffer
-	p.Stdout = os.Stdout
-	p.Stderr = os.Stderr
-	err := p.Run()
-	if err != nil {
-		fmt.Println(err)
-	}
+	TestLISTM(t)
 }
+
+func TestLISTM(t *testing.T) {
+	fmt.Println("Test")
+	go server.Run()
+	conn, err := net.Dial(TYPE, HOST+":"+PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new event
+
+	// Display all the events
+	bufferClientIn := make([]byte, 1024)
+	_, errW := conn.Write([]byte("LISTM "))
+	if errW != nil {
+		log.Fatal(errW)
+	}
+	_, errR := conn.Read(bufferClientIn)
+	bufferClientIn = bytes.Trim(bufferClientIn, "\x00")
+	if errR != nil {
+		log.Fatal(errR)
+	}
+	if string(bufferClientIn) != "Event's id: 0, Event's name: Festival de la musique, Owner: Bob, is open:true\nEvent's id: 1, Event's name: Festival de la bière, Owner: Bob, is open:true" {
+		fmt.Println("Test failed")
+	} else {
+		fmt.Println("Test passed")
+	}
+	fmt.Println(fmt.Sprintf("%s", bufferClientIn))
+
+}
+
+// Test idea
+
+// 1. CREATE an event and do LISTM, check that the new event is there
+
+// 2. CREATE an event, ADD a staff, LISTU and check that the staff was added to the event
+
+// 3. ADD a staff to an event he already is in, check that the users is on the new post and was erase from the old post
+
+// 4. CREATE an event with a post capacity of 1, ADD a person, answer = the person was added, ADD an other person, the answer = "Could not add user to post because post is full"
+
+// 5. CLOSE an event with the authentification of the owner, answer should be = "Event closed"
+
+// 6. CLOSE an event with the authentification of someone else than the owner = "Event couldn't be closed"
+
+// 7. CREATE an event with the wrong authentification, answer should be = "Authentication failed"
